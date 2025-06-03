894ae15e1266c3f887d83373c7298615870ccd8d
Yefu Wang <yefuwang@microsoft.com>
2025-06-04 05:40:01 +0800

    Enable agent framework by default and add temporary depoyment script (#191)

--- src/Deployment/Agent.Web/build_image.ps1
+++ src/Deployment/Agent.Web/build_image.ps1
@@ -1,3 +1,7 @@
+Param(
+    [bool]$IsProd = $true
+)
+
 $ErrorActionPreference = "Stop"
 
 Write-Host "Building and publishing solution"
@@ -5,8 +9,16 @@ dotnet build "..\..\Agent\Agent.Web\Agent.Web.csproj" -c Release --interactive
 dotnet publish "..\..\Agent\Agent.Web\Agent.Web.csproj" --no-build -o out/publish --interactive
 
 Write-Host "Building image"
-$ContainerRegistryEndpoint = "k8seprod.azurecr.io"
+if ($IsProd) {
+    $containerRegistryEndpoint = "k8seprod.azurecr.io"
+    $tag = Get-Date -Format "yyyyMMddHHmmss"
+    Write-Host "Building production image"
+} else {
+    $containerRegistryEndpoint = "sreagent.azurecr.io"
+    $tag = "1.0.224" # This version is enforced by https://github.com/serverless-paas-balam/sreagent-infra/blob/main/.github/workflows/build-deploy.yml#L71
+    Write-Host "Building development image"
+}
 # $AzdEnvName = $(azd env get-value AZURE_ENV_NAME)
-$currentTime = Get-Date -Format "yyyyMMddHHmmss"
-$imageName = "$ContainerRegistryEndpoint/agent-web:$currentTime"
+$imageName = "$containerRegistryEndpoint/agent-web:$tag"
+# $imageName = "$containerRegistryEndpoint/agent-web:$currentTime"
 docker build -t "$imageName" out/publish -f Dockerfile
