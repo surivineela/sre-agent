2adf69a8c070f8915a41bcd642cf704f00171e76
Yefu Wang <yefuwang@microsoft.com>
2025-06-04 18:09:53 +0800

    Add commit hash to non-prod image tag (#213)
    In manual depoyment to AUE, image tag stays the same, making old pods
    not to restart.
    Adding commit hash to non-prod image tag to avoid this issue

--- src/Deployment/Agent.Web/build_image.ps1
+++ src/Deployment/Agent.Web/build_image.ps1
@@ -7,6 +7,7 @@ $ErrorActionPreference = "Stop"
 Write-Host "Building and publishing solution"
 dotnet build "..\..\Agent\Agent.Web\Agent.Web.csproj" -c Release --interactive
 dotnet publish "..\..\Agent\Agent.Web\Agent.Web.csproj" --no-build -o out/publish --interactive
+Copy-Item ".\.dockerignore" -Destination "out/publish" -Force
 
 Write-Host "Building image"
 if ($IsProd) {
@@ -15,7 +16,8 @@ if ($IsProd) {
     Write-Host "Building production image"
 } else {
     $containerRegistryEndpoint = "sreagent.azurecr.io"
-    $tag = "1.0.224" # This version is enforced by https://github.com/serverless-paas-balam/sreagent-infra/blob/main/.github/workflows/build-deploy.yml#L71
+    $gitCommit = (git rev-parse HEAD).Substring(0, 7)
+    $tag = "1.0.224-$gitCommit" # This version prefix is enforced by https://github.com/serverless-paas-balam/sreagent-infra/blob/main/.github/workflows/build-deploy.yml#L71
     Write-Host "Building development image"
 }
 # $AzdEnvName = $(azd env get-value AZURE_ENV_NAME)
