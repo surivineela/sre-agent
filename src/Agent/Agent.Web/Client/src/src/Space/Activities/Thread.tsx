35fd7aa0b80a20ba336d2ce0ffc73a39223f3b14
Qi Liu <liuqi@microsoft.com>
2026-01-23 14:16:22 -0800

    Revert "[UX] Default landing on welcome thread when nothing is selected and welcome thread is created last 24 hours" (#4315)
    Reverts serverless-paas-balam/sreagent-runtime#4307

--- src/Agent/Agent.Web/Client/src/src/Space/Activities/Thread.tsx
+++ src/Agent/Agent.Web/Client/src/src/Space/Activities/Thread.tsx
@@ -1,11 +1,12 @@
 import { FC, useContext, useEffect, useMemo, useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import AzPortalProxy from '../../Common/AzPortalProxy/AzPortalProxy';
+import { AzPortalContext } from '../../Common/AzPortalProxy/Providers/AzPortalProxyContext';
 import { EnvironmentContext } from '../../Common/AzPortalProxy/Providers/StartupInfoContext';
 import { ThreadClient } from '../../Common/Clients/ThreadClient';
 import NoAccessError from '../../Common/Components/NoAccessError';
 import { PermissionActions } from '../../Common/Contracts/Azure/Permission';
-import { Thread as ThreadObject } from '../../Common/Contracts/DataPlane/Thread';
+import { Thread as ThreadObject, ThreadSource } from '../../Common/Contracts/DataPlane/Thread';
 import { SettingNames, useConfigSetting } from '../../Common/Hooks/ConfigSettings';
 import useUserPermissions from '../../Common/Hooks/useUserPermissions';
 import { ChatBoxHandleRef } from '../Contracts/Activities';
@@ -13,8 +14,10 @@ import { SreAgentContext, SreAgentSpaceContext } from '../Contracts/Context';
 import { TracePanel } from '../Foundry/app/components/shell/playground/tracing/TracePanel';
 import { useThreadContentTitleToDoPlanButton } from '../Hooks/useThreadContentTitleToDoPlanButton';
 import { ThreadContentStyles } from '../Styles/Activities.styles';
+import { getNavItemIdFromPathName } from '../Utilities';
 import ChatBox from './ChatBox';
 import ThreadContentTitle from './ThreadContentTitle';
+import { isThreadUnread } from './Utility';
 
 const inStandaloneMode = AzPortalProxy.inStandaloneMode;
 
@@ -24,6 +27,7 @@ interface IThreadProps {
 
 const Thread: FC<IThreadProps> = ({ isOverview }) => {
     const { threadId } = useParams();
+    const location = useLocation();
 
     const { agentObj } = useContext(SreAgentContext);
     const {
@@ -39,6 +43,7 @@ const Thread: FC<IThreadProps> = ({ isOverview }) => {
         subscribeThreadFavoriteUpdate,
     } = useContext(SreAgentSpaceContext);
     const { sreAgentEndpoint, resourceId } = useContext(EnvironmentContext);
+    const proxy = useContext(AzPortalContext);
     const { isCrossTenantPortalMode } = useContext(EnvironmentContext);
 
     const { canReadThreads } = useUserPermissions();
@@ -62,6 +67,59 @@ const Thread: FC<IThreadProps> = ({ isOverview }) => {
 
     const { hasToDoPlans, isToDoPlanOpen, openToDoPlan, closeToDoPlan, setHasToDoPlans, onOpenSidePanel, onCloseSidePanel } =
         useThreadContentTitleToDoPlanButton(chatboxHandleRef);
+
+    useEffect(() => {
+        let isSubscribed = true;
+
+        const selectedNavItem = getNavItemIdFromPathName(location.pathname);
+        if (!selectedNavItem) {
+            const checkForWelcomeThread = async () => {
+                // Get the welcome thread for the last 24 hours if it exists
+                const timestampCutoff = new Date(Date.now() - 1 * 24 * 60 * 60 * 1000).toISOString();
+
+                const threadsResponse = await threadClient.getThreads({
+                    skip: 0,
+                    top: 1,
+                    orderBy: 'modifiedTimestamp',
+                    descending: true,
+                    filters: {
+                        sources: [ThreadSource.welcomeMessage],
+                        timestamps: {
+                            min: {
+                                timestamp: timestampCutoff,
+                                inclusive: true,
+                            },
+                        },
+                    },
+                });
+
+                if (isSubscribed && threadsResponse.isSuccessful && threadsResponse.content && threadsResponse.content.length > 0) {
+                    const welcomeThread = threadsResponse.content[0];
+
+                    if (isThreadUnread(welcomeThread)) {
+                        selectThread(welcomeThread.id);
+
+                        proxy.logAmplitudeOperationEvent({
+                            targetType: 'load',
+                            targetAction: 'loaded',
+                            targetName: 'autoLoadWelcomeThread',
+                            targetFriendlyName: 'Auto-load welcome thread',
+                            metadata: {
+                                threadId: welcomeThread.id,
+                                threadType: ThreadSource.welcomeMessage,
+                            },
+                        });
+                    }
+                }
+            };
+
+            checkForWelcomeThread();
+        }
+
+        return () => {
+            isSubscribed = false;
+        };
+    }, [location.pathname, proxy, selectThread, threadClient]);
 
     useEffect(() => {
         let isSubscribed = true;
